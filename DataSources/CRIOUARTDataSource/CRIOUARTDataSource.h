e657d7868c3646c6cbbea1f193f09e7275b31b0e
André Neto <Andre.Neto@f4e.europa.eu>
2018-06-25 15:54:50 +0200

    First version setup of structure.

---
+++ DataSources/CRIOUARTDataSource/CRIOUARTDataSource.h
@@ -0,0 +1,61 @@
+/**
+ * @file CRIOUARTDataSource.h
+ * @brief Header file for class CRIOUARTDataSource
+ * @date 25/06/2018
+ * @author Marta Baldris
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This header file contains the declaration of the class CRIOUARTDataSource
+ * with all of its public, protected and private members. It may also include
+ * definitions for inline methods which need to be visible to the compiler.
+ */
+
+#ifndef CRIOUARTDATASOURCE_CRIOUARTDATASOURCE_H_
+#define CRIOUARTDATASOURCE_CRIOUARTDATASOURCE_H_
+
+/*---------------------------------------------------------------------------*/
+/*                        Standard header includes                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                        Project header includes                            */
+/*---------------------------------------------------------------------------*/
+#include "Object.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Class declaration                               */
+/*---------------------------------------------------------------------------*/
+
+/**
+ * @brief TODO
+ */
+class CRIOUARTDataSource: public MARTe::Object {
+
+    /**
+     * @brief TODO
+     */
+    CRIOUARTDataSource();
+
+    /**
+     * @brief TODO
+     */
+    virtual ~CRIOUARTDataSource();
+};
+
+/*---------------------------------------------------------------------------*/
+/*                        Inline method definitions                          */
+/*---------------------------------------------------------------------------*/
+
+#endif /* CRIOUARTDATASOURCE_CRIOUARTDATASOURCE_H_ */
+
